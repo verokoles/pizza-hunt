b806f60ccabbcac12dcc343a74724149b0055477
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-06-01 23:31:47 -0500

    comment routes

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -3,21 +3,39 @@ const { Pizza } = require('../models');
 const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
+    //populate comments
     Pizza.find({})
-      .then(dbPizzaData => res.json(dbPizzaData))
+    .populate({
+      path: 'comments',
+      select: '-__v'
+    })
+    .select('-__v')
+    .sort({_id: -1 })
+  .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
-        res.sendStatus(400);
-      });
-  },
+        res.status(400).json(err);
+    });
+},
 
   // get one pizza by id
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
-      .then(dbPizzaData => res.json(dbPizzaData))
+    .populate({
+      path: 'comments',
+      select: '-__v'
+    })
+    .select('-__v')
+    .then(dbPizzaData => {
+      if (!dbPizzaData) {
+        res.status(404).json({ message: "No pizza with this id!"});
+        return;
+      }
+      res.json(dbPizzaData);
+    })
       .catch(err => {
         console.log(err);
-        res.sendStatus(400);
+        res.status(400).json(err);
       });
   },
 
